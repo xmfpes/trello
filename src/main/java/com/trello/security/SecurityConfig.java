a9ce8178573c3f2eb78d03987c24b259244426a1
KyuNam <xmfpes@naver.com>
2017-11-28 17:37:20 +0900

    Feat(SecurityConfig) - create security config

---
+++ src/main/java/com/trello/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.trello.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(WebSecurity web) throws Exception
+	{
+		web.ignoring().antMatchers("/css/**", "/script/**", "image/**", "/fonts/**", "lib/**");
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		http.authorizeRequests()
+			.antMatchers("/admin/**").access("ROLE_ADMIN")
+			.antMatchers("/**").permitAll();
+	}
+}
