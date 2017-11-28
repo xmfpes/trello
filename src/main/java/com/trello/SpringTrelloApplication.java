b3b9240cee6643a3bb33775bd62cd82c621e0ef7
KyuNam <xmfpes@naver.com>
2017-11-28 17:30:44 +0900

    Feat(initialize) - proejct init

---
+++ src/main/java/com/trello/SpringTrelloApplication.java
@@ -0,0 +1,12 @@
+package com.trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrelloApplication.class, args);
+	}
+}
